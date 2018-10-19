1f3903a54d81b0ea46bc3183103bf7a8dbf99151
Levy <leskeylevy@gmail.com>
2018-10-19 14:48:17 +0300

    tried the two stepverificatin but it not working

--- majirani/settings.py
+++ majirani/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'neighbourhood.apps.NeighbourhoodConfig',
+    'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
