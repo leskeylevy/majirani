bae1019bbb3779bd9dfd2787656f7110d0196eea
Levy <leskeylevy@gmail.com>
2018-10-21 09:12:23 +0300

    created a form for editing profiles

--- neighbourhood/admin.py
+++ neighbourhood/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Profile,Neighbourhood,Business
+
 
 # Register your models here.
+admin.site.register(Neighbourhood)
+admin.site.register(Profile)
+admin.site.register(Business)
