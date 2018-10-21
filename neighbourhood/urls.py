da72f13b224bfb0171b01df36dc5b5fda233d8aa
Levy <leskeylevy@gmail.com>
2018-10-21 10:10:25 +0300

    configured mediastorage path

--- neighbourhood/urls.py
+++ neighbourhood/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
     url(r'^$', views.index, name='index'),
@@ -8,3 +10,5 @@ urlpatterns=[
         views.activate, name='activate'),
     url(r'^accounts/profile',views.profile,name='profile'),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
