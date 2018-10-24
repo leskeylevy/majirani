25d3fa5c122315e2f439325cea1b22864bb0cde8
Levy <leskeylevy@gmail.com>
2018-10-24 10:24:53 +0300

    fixed errors in heroku

--- neighbourhood/models.py
+++ neighbourhood/models.py
@@ -53,3 +53,17 @@ class Post(models.Model):
     user = models.ForeignKey(Profile)
     Text = models.CharField(max_length=250)
     neighbourhood = models.ForeignKey(Neighbourhood, related_name='posts')
+
+
+class Location(models.Model):
+    name = models.CharField(max_length=30)
+
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
+
+    def __str__(self):
+        return self.name
